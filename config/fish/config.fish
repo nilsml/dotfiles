7c70dc24c2b6b40c017278193e8b51a17b454695
Nils Magne Lunde <nilsml@nilsland.com>
2020-02-24 19:53:37 +0100

    Updated files

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,4 +1,8 @@
 set -g -x PATH /usr/local/bin $PATH
+set -x PATH $HOME/.fastlane/bin $PATH
+set -x PATH /usr/local/opt/ruby/bin $PATH
+set -x PATH /usr/local/lib/ruby/gems/2.6.0/bin $PATH
+set -g fish_user_paths "/usr/local/opt/ruby/bin" $fish_user_paths
 
 function fish_prompt
   env FISH_VERSION=$FISH_VERSION PROMPTLINE_LAST_EXIT_CODE=$status bash ~/.shell_prompt.sh left
---
+++ config/powershell/profile.ps1
@@ -0,0 +1,2 @@
+
+Import-Module '/Users/nilsml/.local/share/powershell/Modules/posh-git/1.0.0/posh-git.psd1'
